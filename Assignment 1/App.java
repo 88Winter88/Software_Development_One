ddcf90a641f000eff0215f25c96efde9062414bc
88Winter88 <katelynviglietta@gmail.com>
2026-01-29 11:27:47 -0500

    Update App.java

--- Assignment 1/App.java
+++ Assignment 1/App.java
@@ -1,12 +1,16 @@
 //always start with importing our scanner so we can use it!
 import java.util.Scanner;
-public class Debug {
+public class App {
 public static void main(String[] args) {
+Scanner Input= new Scanner (System.in);
 //finish this one for me by receiving and printing the user's age back to them
 System.out.println("How old are you? ");
+Integer age = Input.nextInt();
+System.out.println ("You are " + age + " years old.");
 //can you do some math for me? take these variables and print out the sum of them!
 int num1 = 10;
 int num2 = 37;
+System.out.println ("The sum of these numbers is " + (num1 + num2) + ".");
 //?
 }
 }
